c6cf64ded1fb58728bfaaa093d48e83b13110348
Laima <laima.sleiere@accenture.com>
2025-01-02 15:56:52 +0200

    Add Spaceships.js component

---
+++ app/components/Spaceships.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Spaceships = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Spaceships Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'grey',
+  },
+});
+
+export default Spaceships;
